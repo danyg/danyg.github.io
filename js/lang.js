afbe06f09b0dfcfee4270a6e1925a5458ff0b2ab
danyg <dalgo86@gmail.com>
2024-11-28 13:16:24 +0100

    fix lang

--- js/lang.js
+++ js/lang.js
@@ -1,114 +1,120 @@
 /**
  * @author Daniel Goberitz <danyg>
  */
-(function() {
+(function () {
+  const iconByLang = {
+    en: "gb",
+    es: "es",
+  };
 
-	const iconByLang = {
-		'en': 'gb',
-		'es': 'es'
-	};
+  class Lang {
+    constructor() {
+      this.createdLangs = [];
+      onReady(this.onDomReady.bind(this));
+    }
 
-	class Lang {
-		constructor() {
-			onReady(this.onDomReady.bind(this));
-		}
+    onDomReady() {
+      this.start();
+    }
 
-		onDomReady() {
-			this.start();
-		}
+    start() {
+      this.body = document.body;
+      this.showLangSelector();
+      this.readQS();
+      this.showRequestedLang();
+    }
 
-		start() {
-			this.body = document.body;
-			this.showLangSelector();
-			this.readQS();
-			this.showRequestedLang();
-		}
+    readQS() {
+      this.qs = {};
+      window.location.search
+        .substring(1)
+        .split("&")
+        .forEach((pair) => {
+          const kv = pair.split("=");
+          this.qs[kv[0]] = kv[1];
+        });
+    }
 
-		readQS() {
-			this.qs = {};
-			window.location.search.substring(1).split('&').forEach((pair) => {
-				const kv = pair.split('=');
-				this.qs[kv[0]] = kv[1];
-			});
-		}
+    showRequestedLang() {
+      this.showLang(this.getRequestedLang());
+    }
 
-		showRequestedLang() {
-			this.showLang( this.getRequestedLang() );
-		}
+    getRequestedLang() {
+      return this.qs.hasOwnProperty("lang") ? this.qs.lang : document.body.parentElement.lang;
+    }
 
-		getRequestedLang() {
-			return this.qs.hasOwnProperty('lang') ?
-				this.qs.lang :
-				document.body.parentElement.lang
-			;
-		}
+    showLang(lang) {
+      document.body.querySelectorAll(`[lang]`).forEach((itm) => {
+        itm.style.display = itm.getAttribute("lang") === lang ? "" : "none";
+      });
+    }
 
-		showLang(lang) {
-			document.body.querySelectorAll(`[lang]`).forEach(itm => {
-				itm.style.display =	itm.getAttribute('lang') === lang ? '' : 'none';
-			});
-		}
+    showLangSelector() {
+      this.checkAvailableLanguages();
+      const selector = this.buildSelector();
+      console.log(this.availableLangs);
+      this.availableLangs.forEach((lang) => {
+        if (this.createdLangs.includes(lang)) return;
+        this.createdLangs.push(lang);
 
-		showLangSelector() {
-			this.checkAvailableLanguages();
-			const selector = this.buildSelector();
-			this.availableLangs.forEach((lang) => {
-				const btn = this.createButton();
-				btn.appendChild(this.createFlag(lang));
-				btn.addEventListener('click', this.changeLang.bind(this, lang));
-				selector.appendChild(btn);
-			});
-		}
+        const btn = this.createButton();
+        btn.appendChild(this.createFlag(lang));
+        btn.addEventListener("click", this.changeLang.bind(this, lang));
+        selector.appendChild(btn);
+      });
+    }
 
-		buildSelector() {
-			const selector = document.createElement('dl');
-			selector.className = 'selector';
-			this.body.appendChild(selector);
-			return selector;
-		}
+    buildSelector() {
+      if (this._selector) return this._selector;
 
-		createButton() {
-			const selector = document.createElement('dd');
-			selector.className = 'lang';
-			return selector;
-		}
+      this._selector = document.createElement("dl");
+      this._selector.className = "selector";
+      this.body.appendChild(this._selector);
 
-		createFlag(lang) {
-			//<i class="flag-icon flag-icon-es"></i>
-			const icon = this.getIconPerLang(lang);
-			const flag = document.createElement('i');
-			flag.className = `flag-icon flag-icon-${icon}`;
-			return flag;
-		}
+      return this._selector;
+    }
 
-		checkAvailableLanguages() {
-			this.availableLangs = [];
-			this.body.querySelectorAll(`[lang]`).forEach(itm => {
-				const lang = itm.lang;
-				if(!this.availableLangs.includes(lang)) {
-					this.availableLangs.push(lang);
-				}
-			});
-		}
+    createButton() {
+      const selector = document.createElement("dd");
+      selector.className = "lang";
+      return selector;
+    }
 
-		getIconPerLang(lang) {
-			if(iconByLang.hasOwnProperty(lang)) {
-				return iconByLang[lang];
-			}
-			throw new TypeError(`No icon found for language ${lang}`);
-		}
+    createFlag(lang) {
+      //<i class="flag-icon flag-icon-es"></i>
+      const icon = this.getIconPerLang(lang);
+      const flag = document.createElement("i");
+      flag.className = `flag-icon flag-icon-${icon}`;
+      return flag;
+    }
 
-		changeLang(lang) {
-			const l = window.location;
-			const state = {
-				lang
-			};
-			const url = `${l.protocol}//${l.host}${l.pathname}?lang=${lang}`;
-			window.history.pushState(state, window.title, url);
-			this.showLang(lang);
-		}
-	}
+    checkAvailableLanguages() {
+      this.availableLangs = [];
+      this.body.querySelectorAll(`[lang]`).forEach((itm) => {
+        const lang = itm.lang;
+        if (!this.availableLangs.includes(lang)) {
+          this.availableLangs.push(lang);
+        }
+      });
+    }
 
-	window.lang = new Lang();
+    getIconPerLang(lang) {
+      if (iconByLang.hasOwnProperty(lang)) {
+        return iconByLang[lang];
+      }
+      throw new TypeError(`No icon found for language ${lang}`);
+    }
 
+    changeLang(lang) {
+      const l = window.location;
+      const state = {
+        lang,
+      };
+      const url = `${l.protocol}//${l.host}${l.pathname}?lang=${lang}`;
+      window.history.pushState(state, window.title, url);
+      this.showLang(lang);
+    }
+  }
+
+  window.lang = new Lang();
 })();
