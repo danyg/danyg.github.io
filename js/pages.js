e6d9fa6616dd2a8518c7a760b0871cf984b8c475
danyg <dalgo86@gmail.com>
2024-11-28 13:21:15 +0100

    improve printability

--- js/pages.js
+++ js/pages.js
@@ -1,74 +1,102 @@
 /**
  * @author Daniel Goberitz <danyg>
  */
-(function() {
-	class Pages {
-		constructor() {
-			onReady(this.onDomReady.bind(this));
-		}
+(function () {
+  class Pages {
+    constructor() {
+      onReady(this.onDomReady.bind(this));
+      window.addEventListener("beforeprint", this.onBeforePrint.bind(this));
+      window.addEventListener("afterprint", this.onAfterPrint.bind(this));
+    }
 
-		onDomReady() {
-			this.body = document.body;
-			this.addClass(this.body, 'with-pages');
-			this.createPages();
-		}
+    isPrintingMode() {
+      return window.matchMedia("print").matches;
+    }
 
-		createPages() {
-			let remainingSections = this.getSections();
-			do {
-				remainingSections = this.createPage(remainingSections);
-			} while(remainingSections.length > 0);
-		}
+    onDomReady() {
+      this.start();
+    }
 
-		createPage(sectionsToAllocate) {
-			const page = this.buildPage();
-			const maxHeight = page.offsetHeight;
-			let currentHeight = 0;
-			const remainingSections = [].concat(sectionsToAllocate);
-			let lastSection;
+    start() {
+      if (this.isPrintingMode()) return;
 
-			for(let section of sectionsToAllocate) {
-				const h = this.getOuterHeightOf(section);
-				if(currentHeight + h < maxHeight || h > maxHeight) {
-					page.appendChild(section);
-					currentHeight += h;
-					remainingSections.splice(0,1);
-					lastSection = section;
-					continue;
-				}
-				if(lastSection) {
-					this.addClass(lastSection, 'page-breaker');
-				}
-				break;
-			}
+      this.body = document.body;
+      this.addClass(this.body, "with-pages");
+      this.createPages();
+    }
 
-			return remainingSections;
-		}
+    onBeforePrint() {
+      console.log("onBeforePrint");
+      const pages = Array.from(document.querySelectorAll(".din-a4"));
 
-		buildPage() {
-			const page = document.createElement('div');
-			page.className = 'din-a4 page-breaker';
-			this.body.appendChild(page);
-			return page;
-		}
+      pages
+        .map((page) => Array.from(page.children))
+        .flat()
+        .forEach((elm) => document.body.appendChild(elm));
 
-		getSections() {
-			return Array.from(this.body.querySelectorAll('section'));
-		}
+      pages.forEach((p) => p.parentElement.removeChild(p));
+    }
 
-		getOuterHeightOf(el) {
-			// from http://youmightnotneedjquery.com/
-			let height = el.offsetHeight;
-			const style = getComputedStyle(el);
+    onAfterPrint() {
+      this.start();
+    }
 
-			height += parseInt(style.marginTop) + parseInt(style.marginBottom);
-			return height;
-		}
+    createPages() {
+      let remainingSections = this.getSections();
+      do {
+        remainingSections = this.createPage(remainingSections);
+      } while (remainingSections.length > 0);
+    }
 
-		addClass(el, className) {
-			el.className += (el.className.length > 0 ? ' ' : '') + className;
-		}
-	}
+    createPage(sectionsToAllocate) {
+      const page = this.buildPage();
+      const maxHeight = page.offsetHeight;
+      let currentHeight = 0;
+      const remainingSections = [].concat(sectionsToAllocate);
+      let lastSection;
 
-	window.pages = new Pages();
+      for (let section of sectionsToAllocate) {
+        const h = this.getOuterHeightOf(section);
+        if (currentHeight + h < maxHeight || h > maxHeight) {
+          page.appendChild(section);
+          currentHeight += h;
+          remainingSections.splice(0, 1);
+          lastSection = section;
+          continue;
+        }
+        if (lastSection) {
+          this.addClass(lastSection, "page-breaker");
+        }
+        break;
+      }
+
+      return remainingSections;
+    }
+
+    buildPage() {
+      const page = document.createElement("div");
+      page.className = "din-a4 page-breaker";
+      this.body.appendChild(page);
+      return page;
+    }
+
+    getSections() {
+      return Array.from(this.body.querySelectorAll("section"));
+    }
+
+    getOuterHeightOf(el) {
+      // from http://youmightnotneedjquery.com/
+      let height = el.offsetHeight;
+      const style = getComputedStyle(el);
+
+      height += parseInt(style.marginTop) + parseInt(style.marginBottom);
+      return height;
+    }
+
+    addClass(el, className) {
+      el.className += (el.className.length > 0 ? " " : "") + className;
+    }
+  }
+
+  window.pages = new Pages();
 })();
